a521e23f1ada78697a9da40a8dc87b5f9d4e847f
hema-code20 <hemavathisiva05@gmail.com>
2024-07-25 10:39:11 +0530

    commit simple qrcode generator using python

---
+++ qrcodescanner.py
@@ -0,0 +1,6 @@
+import qrcode 
+features = qrcode.QRCode(version=1, box_size=40, border=2)
+features.add_data('https://github.com/hema-code20')
+features.make(fit=True)
+generate_image = features.make_image(fill_color="teal", back_color="white")
+generate_image.save('qrcode.png')
