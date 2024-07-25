862b7b3f1f386dce5091de23fe0f78f7889cfef0
hema-code20 <hemavathisiva05@gmail.com>
2024-07-25 10:52:20 +0530

    updated

--- qrcodescanner.py
+++ qrcodescanner.py
@@ -1,5 +1,5 @@
 import qrcode 
-features = qrcode.QRCode(version=1, box_size=40, border=2)
+features = qrcode.QRCode(version=1, box_size=10, border=2)
 features.add_data('https://github.com/hema-code20')
 features.make(fit=True)
 generate_image = features.make_image(fill_color="teal", back_color="white")
